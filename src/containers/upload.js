c2a9d7377b295faee966c11835f9fe9d4d7b5b91
Mohammed Mosayed <mohammedmosayed@gmail.com>
2019-03-12 21:23:20 -0400

    final

--- src/containers/upload.js
+++ src/containers/upload.js
@@ -1,6 +1,51 @@
 import React from 'react';
+import * as firebase from 'firebase';
+import ImageService from '../services/images';
+
+  // Initialize Firebase
+ const config = {
+    apiKey: "AIzaSyDnX-V1IEQDlgNv1XwraQA9jXi3pE8Yomk",
+    authDomain: "dumb-uploader.firebaseapp.com",
+    databaseURL: "https://dumb-uploader.firebaseio.com",
+    projectId: "dumb-uploader",
+    storageBucket: "dumb-uploader.appspot.com",
+    messagingSenderId: "370924651634"
+};
+firebase.initializeApp(config);
 
 export default class Home extends React.Component {
+
+  saveImage = (url) => {
+    const date = Date();
+
+    ImageService.saveImage(url, date);
+  }
+
+  handleFileInput = async (e) => {
+    const firstFile = e.target.files[0];
+
+    const root = firebase.storage().ref()
+    const newImage = root.child(firstFile.name);
+
+    // newImage.put(firstFile)
+    //   .then((snapshot) => {
+    //     return snapshot.ref.getDownloadURL()
+    //   })
+    //   .then((url) => {
+    //     console.log(url)
+    //     this.saveImage(url);
+    //   })
+
+    try {
+      const snapshot = await newImage.put(firstFile);
+      const url = await snapshot.ref.getDownloadURL();
+      this.saveImage(url);
+    }
+    catch(err) {
+      console.log(err);
+    }
+    
+  }
 
   render() {
 
@@ -8,7 +53,7 @@ export default class Home extends React.Component {
       <div className='container'>
         <div className="input-group mb-3">
           <div className="custom-file">
-            <input type="file" className="custom-file-input" />
+            <input type="file" className="custom-file-input" onChange={this.handleFileInput} />
             <label className="custom-file-label">Upload Image</label>
           </div>
         </div>
